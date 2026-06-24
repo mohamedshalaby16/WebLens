bccae12392bfd9b95f9bbd69e318df682eab7222
MohamedShalaby <Mohamed.Shalaby04@eng-st.cu.edu.eg>
2026-06-24 15:30:56 +0300

    Report PDF feat

---
+++ python/report_generator.py
@@ -0,0 +1,11 @@
+import io
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.units import cm
+from reportlab.lib import colors
+from reportlab.platypus import (
+    SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, HRFlowable
+)
+from reportlab.lib.enums import TA_CENTER, TA_LEFT, TA_JUSTIFY
+
+from models import WebLensReport
