3da20551c930a26748e3818ed5fb0be9b99fd363
MohamedShalaby <Mohamed.Shalaby04@eng-st.cu.edu.eg>
2026-06-25 14:36:52 +0300

    Initializing UI

---
+++ csharp/WebLens.Desktop/Models/CloneResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLens.Desktop.Models
+{
+    public class CloneResponse
+    {
+        public string JobId { get; set; } = "";
+        public string Status { get; set; } = "";
+        public string Url { get; set; } = "";
+        public string Timestamp { get; set; } = "";
+    }
+}
